55609542719d88ec6a4364f62b5aaaebf8b12f13
ci-work <ci@work>
2022-07-01 00:49:11 +0100

    ..

--- src/transactions/v2/blockchain_txn_poc_receipts_v2.erl
+++ src/transactions/v2/blockchain_txn_poc_receipts_v2.erl
@@ -673,7 +673,7 @@ validate(_POCVersion, Txn, Path, LayerData, LayerHashes, OldLedger) ->
                                                        {error, invalid_receipt}
                                                end;
                                            _Unknown ->
-                                               lager:error([{poc_id, POCID}], "receipt not in order, ~p ~p", [libp2p_crypto:pubkey_bin_to_p2p(Elem), libp2p_crypto:pubkey_bin_to_p2p(Gateway)]),
+                                               lager:error([{poc_id, POCID}], "receipt not in order, ~p ~p", [Elem, Gateway]),
                                                {error, receipt_not_in_order}
                                        end
                                end,
