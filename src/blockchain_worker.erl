d271b44ff650dc94f952a1576e66d0d49f8d0eb7
ci-work <ci@work>
2022-08-15 11:18:04 +0100

    remove fixed peer lookups

--- src/blockchain_worker.erl
+++ src/blockchain_worker.erl
@@ -950,7 +950,7 @@ reset_ledger_to_snap(Hash, Height, State) ->
     snapshot_sync(State1#state{snapshot_info=SnapInfo}).
 
 start_sync(#state{blockchain = Chain, swarm_tid = SwarmTID} = State) ->
-    case get_fixed_peer(SwarmTID) of
+    case get_random_peer(SwarmTID) of
         no_peers ->
             %% try again later when there's peers
             schedule_sync(State);
@@ -960,26 +960,6 @@ start_sync(#state{blockchain = Chain, swarm_tid = SwarmTID} = State) ->
                        [Pid, Ref, RandomPeer]),
             State#state{sync_pid = Pid, sync_ref = Ref}
     end.
-
--spec get_fixed_peer(SwarmTID :: ets:tab()) -> string().
-get_fixed_peer(SwarmTID) ->
-    RandomSourceAddr = lists:nth(rand:uniform(2), [get_fixed_source_peer(SwarmTID), get_random_peer(SwarmTID)]),
-    lager:info("selected random peer address ~p", [RandomSourceAddr]),
-    RandomSourceAddr.
-
--spec get_fixed_source_peer(SwarmTID :: ets:tab()) -> string().
-get_fixed_source_peer(SwarmTID) ->
-    LocalAddr = libp2p_swarm:p2p_address(SwarmTID),
-    ExcludedAddrs = [LocalAddr],
-    FixedAddrs = case application:get_env(blockchain, fixed_sources) of
-        {ok, ""} -> [];
-        {ok, Nodes} -> string:split(Nodes, ",", all);
-        _ -> []
-    end,
-    BaseAddrs = sets:to_list(sets:subtract(sets:from_list(FixedAddrs), sets:from_list(ExcludedAddrs))),
-    RandomSourceAddr = lists:nth(rand:uniform(length(BaseAddrs)), BaseAddrs),
-    lager:info("suggesting fixed_sources address ~p", [RandomSourceAddr]),
-    RandomSourceAddr.
 
 -spec get_random_peer(SwarmTID :: ets:tab()) -> no_peers | string().
 get_random_peer(SwarmTID) ->
